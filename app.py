0d96fa59ee7fee09c3ba1e5546d630334886cacd
NkBots <116135526+NkBots@users.noreply.github.com>
2023-01-01 23:16:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ def hello_world():
    return 'Hello, World!'
 
 #Ex https://Itz-zaid:ghp_147bkkabcdefgh@github.com/Itz-zaid/anywhere
-os.system("git clone https://nkbots:token@github.com/nkbots/testre ok && cd ok && pip3 install -U -r requirements.txt && nohup python3 main.py &")
+os.system("git clone https://github.com/nkbots/testre ok && cd ok && pip3 install -U -r requirements.txt && nohup python3 main.py &")
